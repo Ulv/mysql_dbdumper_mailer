80a2aa0b57c21eddd221be04342e1b687ec3ca70
Vladimir Chmil <ulv8888@gmail.com>
2013-10-17 18:46:56 +0300

    send dump in attach.

--- DbDumperMailer.php
+++ DbDumperMailer.php
@@ -30,19 +30,19 @@ class DbDumperMailer extends DbDumper
     protected $email = "vladimir.chmil@gmail.com";
 
     /**
-     * @param string $email
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
-
-    /**
      * @return string
      */
     public function getEmail()
     {
         return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
     }
 
     protected function start()
@@ -53,8 +53,33 @@ class DbDumperMailer extends DbDumper
     protected function finish()
     {
         gzclose($this->ex_gz);
-        var_dump($this->tmpf);
-//        readfile($this->tmpf);
-//        flush();
+
+        $mailer = new PHPMailer(true);
+        $mailer->setFrom($this->email);
+        $mailer->CharSet  = "Windows-1251";
+        $mailer->WordWrap = 80;
+
+        $mailer->addAddress($this->email);
+
+        $mailer->Subject = "[" . date('H:i d/m/Y') . "] Дамп базы";
+
+        $mbody = <<<EOD
+Здравствуйте
+
+Во вложении дамп базы от %s
+EOD;
+
+        $mbody = sprintf($mbody, date('H:i d/m/Y'));
+
+        $mailer->msgHTML(nl2br($mbody));
+        $mailer->AltBody = $mbody;
+        $mailer->AddAttachment(
+            $this->tmpf,
+            "masterovoy-" . date('d-m-Y-h-i-s') . ".sql.gz"
+//            "base64",
+//            "application/x-gzip"
+        );
+
+        $mailer->send();
     }
 }
